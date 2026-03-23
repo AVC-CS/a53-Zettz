f3d0f91583e454aa88a8817d0a939f55431b53b6
Lorenzo <calderonlorenzo026@gmail.com>
2026-03-23 21:21:21 +0000

    started a53 code

--- main.cpp
+++ main.cpp
@@ -1,11 +1,33 @@
 #include <iostream>
-#include <iomanip>
 using namespace std;
 
 int main()
 {
-    int i, begin, end, sum = 0;
+    int begin, end, i, total=0;
+    while(1) {
+      cout << " Enter begin and end ";
+      cin >> begin >> end;
+      if (begin < end)
+        break;
+    }
 
-    // TODO: prompt user for begin and end values
-    // TODO: loop from begin to end and accumulate sum of even values
+
+
+    //int flag = 1;
+    //while(flag) {
+        //cout << " Enter begin and end again ";
+        //cin >> begin >> end;
+        //if (begin < end)
+        //flag = 0;
+    
+      //}
+    if ( begin % 2 == 1)
+        begin++;
+    i = begin; // 10
+    while ( i <= end){
+        total = total + i;
+        i += 2;
+    }
+    cout << "Total is " << total << endl;
+    return 0;
 }
